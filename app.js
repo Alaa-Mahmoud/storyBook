78219ef014029ea53b19d506817a5c9ea703aa62
Alaa-Mahmoud <alaamahmoud.fci.scu@gmail.com>
2017-12-22 22:16:38 +0200

    handeld auth using google-oauth20

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -8,7 +9,15 @@ app.get('/', (req, res) => {
     res.send('it works');
 });
 
+// passport config 
+require('./config/passport')(passport);
 
+// here to load routes
+const auth = require('./routes/auth');
+
+
+// here to put middleware
+app.use('/auth', auth);
 
 
 
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://alaa:alaa@ds163656.mlab.com:63656/storybooks-dev',
+    googleClientID: '728991525542-49meh08cgdibbmdifou2cvcc0g0r28hd.apps.googleusercontent.com',
+    googleClientSecret: 'OCs-E5-Smt46a_MbM51fAHER',
+};
