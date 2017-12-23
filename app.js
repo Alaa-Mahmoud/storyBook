30556b7799867f0e273dc84922cad9e6dee1188d
Alaa-Mahmoud <alaamahmoud.fci.scu@gmail.com>
2017-12-23 12:30:29 +0200

    start UI design and finish side bar

--- app.js
+++ app.js
@@ -1,30 +1,64 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const passport = require('passport');
-const app = express();
-const PORT = process.env.PORT || 3000;
 
+// Load User Model
+require('./models/user');
 
-app.get('/', (req, res) => {
-    res.send('it works');
-});
-
-// passport config 
+// Passport Config
 require('./config/passport')(passport);
 
-// here to load routes
+// Load Routes
+const index = require('./routes/index');
 const auth = require('./routes/auth');
 
+// Load Keys
+const keys = require('./config/keys');
 
-// here to put middleware
+// Map global promises
+mongoose.Promise = global.Promise;
+// Mongoose Connect
+mongoose.connect(keys.mongoURI, {
+        useMongoClient: true
+    })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+const app = express();
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+app.use(cookieParser());
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+}));
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Set global vars
+app.use((req, res, next) => {
+    res.locals.user = req.user || null;
+    next();
+});
+
+// Use Routes
+app.use('/', index);
 app.use('/auth', auth);
 
 
+const port = process.env.PORT || 3000;
 
-
-
-
-
-app.listen(PORT, () => {
-    console.log(`server started on port ${PORT}`);
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
 });
